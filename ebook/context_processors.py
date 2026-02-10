dc47a40570def2d9152435d431e2600e6aff2b21
Etisha Godle <etishagodle14@example.com>
2026-02-10 16:53:29 +0530

    Fixed wishlist model name in context processor

--- ebook/context_processors.py
+++ ebook/context_processors.py
@@ -1,16 +1,16 @@
-from .models import CartItem, WishlistItem
+from .models import CartItem, Wishlist
 from .models import Category
 
 def category_renderer(request):
     return {
         'categories': Category.objects.all()
     }
-
-
 def nav_counts(request):
     if request.user.is_authenticated:
         return {
+            # User ke cart ke items ka count
             'cart_count': CartItem.objects.filter(user=request.user).count(),
-            'wishlist_count': WishlistItem.objects.filter(user=request.user).count(),
+            # User ki wishlist ke items ka count (Aapke model ke hisaab se)
+            'wishlist_count': Wishlist.objects.filter(user=request.user).count(),
         }
     return {'cart_count': 0, 'wishlist_count': 0}
